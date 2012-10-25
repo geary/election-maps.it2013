24e5a656040a62cf26f37988436e53b2f4a5c151
Michael Geary <mg@mg.to>
2012-10-25 01:46:14 -0700

    Add JavaScript minifier

---
+++ scripts/minify_conf.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+jsFiles = [
+	'polygonzo.js',
+	'scriptino.js',
+	'elections-us.js',
+	'results-map-us.js',
+]
+
+jsMinFile = 'results-map-us-min.js'
+
+jsCopyright = '''\
+// Copyright Michael Geary - http://mg.to/
+// Use under MIT License or Unlicense - see LICENSE for license details
+'''
