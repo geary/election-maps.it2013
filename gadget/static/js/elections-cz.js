f6f3e18848e89a54002ca45ee51d595197bb0035
Michael Geary <mg@mg.to>
2013-01-08 18:04:10 -0800

    Update candidate colors

--- gadget/static/js/elections-cz.js
+++ gadget/static/js/elections-cz.js
@@ -4,20 +4,20 @@
 
 var candidatesCZ2013 = _.map([
 	// "color|id|firstName|lastName|fullName"
-	"#81C57A|1|Zuzana|Roithov\u00E1|Zuzana Roithov\u00E1",
-	"#814A19|2|Jan|Fischer|Jan Fischer",
-	"#2A4BD7|3|Jana|Bobo\u0161\u00EDkov\u00E1|Jana Bobo\u0161\u00EDkov\u00E1",
-	"#8126C0|4|T\u00E1\u0148a|Fischerov\u00E1|T\u00E1\u0148a Fischerov\u00E1",
-	"#29D0D0|5|P\u0159emysl|Sobotka|P\u0159emysl Sobotka",
-	"#FF9233|6|Milo\u0161|Zeman|Milo\u0161 Zeman",
-	"#9DAFFF|7|Vladim\u00EDr|Franz|Vladim\u00EDr Franz",
-	"#1D6914|8|Ji\u0159\u00ED|Dienstbier|Ji\u0159\u00ED Dienstbier Jr.",
-	"#AD2323|9|Karel|Schwarzenberg|Karel Schwarzenberg"
+	"1|#FFEC71|Zuzana|Roithov\u00E1|Zuzana Roithov\u00E1",
+	"2|#093970|Jan|Fischer|Jan Fischer",
+	"3|#D0B4FF|Jana|Bobo\u0161\u00EDkov\u00E1|Jana Bobo\u0161\u00EDkov\u00E1",
+	"4|#B6C431|T\u00E1\u0148a|Fischerov\u00E1|T\u00E1\u0148a Fischerov\u00E1",
+	"5|#52A0FF|P\u0159emysl|Sobotka|P\u0159emysl Sobotka",
+	"6|#782727|Milo\u0161|Zeman|Milo\u0161 Zeman",
+	"7|#84B3B0|Vladim\u00EDr|Franz|Vladim\u00EDr Franz",
+	"8|#E08E21|Ji\u0159\u00ED|Dienstbier|Ji\u0159\u00ED Dienstbier Jr.",
+	"9|#A2289C|Karel|Schwarzenberg|Karel Schwarzenberg"
 ], function( s ) {
 	s = s.split('|');
 	return {
-		color: s[0],
-		id: s[1],
+		id: s[0],
+		color: s[1],
 		firstName: s[2],
 		lastName: s[3],
 		fullName: s[4]
