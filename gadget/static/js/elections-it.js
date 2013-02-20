ff84deb89704e09fa4c9ff3d53958db392b2410a
Michael Geary <mg@mg.to>
2013-02-20 12:27:03 -0800

    Bring up initial Italian map

---
+++ gadget/static/js/elections-it.js
@@ -0,0 +1,51 @@
+// elections-it.js
+// By Michael Geary - http://mg.to/
+// See UNLICENSE or http://unlicense.org/ for public domain notice.
+
+var partiesIT2013 = _.map([
+	// "color|id|firstName|lastName|fullName|round2"
+	"1|#FFEC71|Zuzana|Roithov\u00E1|Zuzana Roithov\u00E1",
+	"2|#093970|Jan|Fischer|Jan Fischer",
+	"3|#D0B4FF|Jana|Bobo\u0161\u00EDkov\u00E1|Jana Bobo\u0161\u00EDkov\u00E1",
+	"4|#B6C431|T\u00E1\u0148a|Fischerov\u00E1|T\u00E1\u0148a Fischerov\u00E1",
+	"5|#52A0FF|P\u0159emysl|Sobotka|P\u0159emysl Sobotka",
+	"6|#9A0E48|Milo\u0161|Zeman|Milo\u0161 Zeman|true",
+	"7|#84B3B0|Vladim\u00EDr|Franz|Vladim\u00EDr Franz",
+	"8|#E08E21|Ji\u0159\u00ED|Dienstbier|Ji\u0159\u00ED Dienstbier Jr.",
+	"9|#3F21D9|Karel|Schwarzenberg|Karel Schwarzenberg|true"
+], function( s ) {
+	s = s.split('|');
+	return {
+		id: s[0],
+		color: s[1],
+		firstName: s[2],
+		lastName: s[3],
+		fullName: s[4],
+		round2: !! s[5]
+	};
+});
+
+
+var elections = {
+	'2013-senate-1': {
+		date: '2013-02-25',
+		tzHour: +1,
+		photos: false,
+		candidates: partiesIT2013,
+		parties: partiesIT2013,
+		electionids: {
+			'IT': 9999
+		}
+	},
+	'2013-deputy-1': {
+		date: '2013-02-25',
+		tzHour: +1,
+		photos: false,
+		candidates: partiesIT2013,
+		parties: partiesIT2013,
+		electionids: {
+			'IT': 9999
+		}
+	}
+};
+
